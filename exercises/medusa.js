9bdad29c42f839d3f76a552ff04b587e6aeb835e
Eli Sachs <easachs13@gmail.com>
2022-11-25 23:53:12 -0700

    Add medusa

---
+++ exercises/medusa.js
@@ -0,0 +1,16 @@
+class Medusa {
+  constructor(name) {
+    this.name = name;
+    this.statues = [];
+  }
+
+  stare(victim) {
+    this.statues.push(victim);
+    victim.stoned = true;
+    if (this.statues.length >= 4) {
+      this.statues.shift();
+    }
+  }
+}
+
+module.exports = Medusa;
